cd711522e99077311f6acbe66848251b7a76b484
Marc Schaar <m-schaar@gmx.net>
2024-08-13 17:36:09 +0200

    evocahin

--- scripts/detail-info/evolution-chain.js
+++ scripts/detail-info/evolution-chain.js
@@ -1,131 +1,140 @@
 // Hauptfunktion, um die Evolution-Kette anzuzeigen
+
+let possibleFirstEvoltions = []
+let possibleSecondEvoltions = []
+
 async function showEvolutionChain(pokemonId) {
-  // Hole die Daten über die Pokémon-Spezies
-  const speciesData = await fetchPokemonSpecies(pokemonId);
-  // Hole die Evolution-Ketten-Daten
-  const evolutionChainData = await fetchEvolutionChain(speciesData.evolution_chain.url);
-  // Leere den Container und erstelle einen neuen
-  clearContainer();
-  createEvolutionContainer(pokemonId);
-  // Zeige die Evolution-Kette an
-  await displayEvolutionChain(evolutionChainData);
+    const speciesData = await fetchPokemonSpecies(pokemonId);
+    const evolutionChainData = await fetchEvolutionChain(speciesData.evolution_chain.url);
+
+    clearContainer();
+    await createEvolutionContainer(pokemonId);
+    await getData(evolutionChainData)
+
 }
 
-// Funktion zum Leeren des Containers
+async function getData(evolutionChainData) {
+    let baseEvolutionOverview = getBaseEvolutionData(evolutionChainData);
+    let URL_FOR_IMG = baseEvolutionOverview.species.url
+    let img = await getEvolutionImg(URL_FOR_IMG)
+    console.log(img);
+    let name = baseEvolutionOverview.species.name;
+    let evolvesTo = baseEvolutionOverview.evolves_to
+    for (let index = 0; index < evolvesTo.length; index++) {
+        let possibleEvoltion = evolvesTo[index];
+        possibleFirstEvoltions.push(possibleEvoltion)
+    }
+    await getFirstEvolutionData()
+    await getSecondEvolutionData()
+    await displayImg(img)
+
+}
+
+
+function getBaseEvolutionData(evolutionChainData) {
+    let baseOverwiev = evolutionChainData.chain
+    return baseOverwiev
+}
+
+async function getFirstEvolutionData() {
+    for (let index = 0; index < possibleFirstEvoltions.length; index++) {
+        const overview = possibleFirstEvoltions[index]
+        const firstEvolutionName = overview.species.name;
+        let evolutionTrigger = overview.evolution_details[0].trigger
+        let evolutionTriggerName = evolutionTrigger.name
+        let evolutionTriggerUrl = evolutionTrigger.url
+        let evolves_to = overview.evolves_to
+        let URL_FOR_IMG = overview.species.url
+        let img = await getEvolutionImg(URL_FOR_IMG)
+        console.log(img);
+
+
+
+        for (let index = 0; index < evolves_to.length; index++) {
+            let possibleEvoltion = evolves_to[index];
+            possibleSecondEvoltions.push(possibleEvoltion)
+
+        }
+    }
+
+}
+
+async function getSecondEvolutionData() {
+    for (let index = 0; index < possibleSecondEvoltions.length; index++) {
+        const overview = possibleSecondEvoltions[index]
+        const secondEvolutionName = overview.species.name;
+        let evolutionTrigger = overview.evolution_details[0].trigger
+        let evolutionTriggerName = evolutionTrigger.name
+        let evolutionTriggerUrl = evolutionTrigger.url
+        let URL_FOR_IMG = overview.species.url
+        let img = await getEvolutionImg(URL_FOR_IMG)
+        console.log(img);
+
+
+
+    }
+}
+
 function clearContainer() {
-  document.getElementById("container").innerHTML = "";
-  document.getElementById("switch").classList.add("d-none");
+    document.getElementById("container").innerHTML = "";
+    document.getElementById("switch").classList.add("d-none");
 }
 
-// Hole die Pokémon-Spezies-Daten
+// // Hole die Pokémon-Spezies-Daten
 async function fetchPokemonSpecies(pokemonId) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`);
-  return await response.json();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`);
+    return await response.json();
 }
 
 // Hole die Evolution-Kette-Daten
 async function fetchEvolutionChain(evoChainURL) {
-  const response = await fetch(evoChainURL);
-  return await response.json();
+    const response = await fetch(evoChainURL);
+    return await response.json();
 }
 
 // Erstelle den Container für die Evolution-Kette
-function createEvolutionContainer(pokemonId) {
-  const containerRef = document.getElementById("container");
-  containerRef.innerHTML = `
+async function createEvolutionContainer(pokemonId) {
+    const containerRef = document.getElementById("container");
+    containerRef.innerHTML = `
 <div class="d-flex align-items-center">
-  <div class="d-flex align-items-end gap-3 w-100">
-    <div onclick="displayChoosenPokemon(${pokemonId})" class="d-flex align-items-center column pointer">
-      <img class="chain" id="evolution1" src="" alt="1" />
-      <span id="name-evo1"></span>
+    <div class="d-flex align-items-end gap-3 w-100">
+        <div id="img-container-base" onclick="displayChoosenPokemon(${pokemonId})" class="d-flex align-items-center column pointer">
+
+        </div>
+        <div class="d-flex column justify-center">
+            <img class="arrow" src="./img/icon/right-arrow.png" alt="arrow" />
+            <span id="first-level-up"></span>
+        </div>
+        <div id="first-evo-container" onclick="displayChoosenPokemon(${pokemonId})" class="d-flex align-items-center column pointer">
+            <img class="chain" id="evolution2" src="" alt="2" />
+            <span id="name-evo2"></span>
+        </div>
+        <div class="d-flex column justify-center">
+            <img class="arrow" src="./img/icon/right-arrow.png" alt="arrow" />
+            <span id="second-level-up"></span>
+        </div>
+        <div onclick="displayChoosenPokemon(${pokemonId})" class="d-flex align-items-center column pointer">
+            <img class="chain" id="evolution3" src="" alt="3" />
+            <span id="name-evo3"></span>
+        </div>
     </div>
-    <div class="d-flex column justify-center">
-      <img class="arrow" src="./img/icon/right-arrow.png" alt="arrow" />
-      <span id="first-level-up"></span>
-    </div>
-    <div id="first-evo-container" onclick="displayChoosenPokemon(${pokemonId})" class="d-flex align-items-center column pointer">
-      
-    </div>
-    <div class="d-flex column justify-center">
-      <img class="arrow" src="./img/icon/right-arrow.png" alt="arrow" />
-      <span id="second-level-up"></span>
-    </div>
-    <div onclick="displayChoosenPokemon(${pokemonId})" class="d-flex align-items-center column pointer">
-      <img class="chain" id="evolution3" src="" alt="3" />
-      <span id="name-evo3"></span>
-    </div>
-  </div>
 </div>
   `;
 }
 
-// Zeige die Evolution-Kette an
-async function displayEvolutionChain(evoChain) {
-  // Hole den Namen und das Bild für die Basis-Evolution
-  let BASE_EVO_URL = evoChain.chain.species.url;
-  await getEvolutionImg(BASE_EVO_URL, 1);
-  displayBaseEvolution(evoChain);
+async function getEvolutionImg(speciesUrl) {
+    const speciesResponse = await fetch(speciesUrl);
+    const speciesData = await speciesResponse.json();
 
-  // Hole den Namen und das Bild für die erste Evolution
-  for (const firstEvolution of evoChain.chain.evolves_to) {
-    let FIRST_EVO_URL = firstEvolution.species.url;
-    await getEvolutionImg(FIRST_EVO_URL, 2);
-    displayFirstEvolution(firstEvolution);
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${speciesData.id}/`);
+    const pokemonData = await pokemonResponse.json();
 
-    // Hole den Namen und das Bild für die zweite Evolution
-    for (const secondEvolution of firstEvolution.evolves_to) {
-      let SECOND_EVO_URL = secondEvolution.species.url;
-      await getEvolutionImg(SECOND_EVO_URL, 3);
-      displaySecondEvolution(secondEvolution);
-    }
-  }
+    const imageUrl = pokemonData.sprites.other.dream_world.front_default;
+    return imageUrl
 }
 
-// Zeige die Basis-Evolution an
-function displayBaseEvolution(evoChain) {
-  // let evoContainerRef = document.getElementById("first-evo-container");
-
-  let firstLevelUp = evoChain.chain.evolves_to[0]?.evolution_details[0]?.min_level;
-  let nameRef1 = document.getElementById("name-evo1");
-  let firstLevelUpRef = document.getElementById("first-level-up");
-  // evoContainerRef.innerHTML += `<img class="chain" id="evolution1" src="" alt="2" />
-  //     <span id="name-evo2"></span>`;
-  nameRef1.innerHTML = evoChain.chain.species.name.toUpperCase();
-  firstLevelUpRef.innerHTML = "Lvl:" + (firstLevelUp || "");
-}
-
-// Zeige die erste Evolution an
-function displayFirstEvolution(firstEvolution) {
-  let nameRef2 = document.getElementById("name-evo2");
-  let secondLevelUpRef = document.getElementById("second-level-up");
-  let secondLevelUp = firstEvolution.evolves_to[0]?.evolution_details[0]?.min_level;
-
-  nameRef2.innerHTML = firstEvolution.species.name.toUpperCase();
-  secondLevelUpRef.innerHTML = "Lvl:" + (secondLevelUp || "");
-}
-
-// Zeige die zweite Evolution an
-function displaySecondEvolution(secondEvolution) {
-  let nameRef3 = document.getElementById("name-evo3");
-  nameRef3.innerHTML = secondEvolution.species.name.toUpperCase();
-}
-
-// Hole die Bild-URL der Evolution und zeige das Bild an
-async function getEvolutionImg(speciesUrl, i) {
-  const speciesResponse = await fetch(speciesUrl);
-  const speciesData = await speciesResponse.json();
-
-  const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${speciesData.id}/`);
-  const pokemonData = await pokemonResponse.json();
-
-  const imageUrl = pokemonData.sprites.other.dream_world.front_default;
-
-  displayEvolutionImg(imageUrl, i);
-}
-
-// Setze die Bild-URL für die Evolution auf das Bild-Element
-function displayEvolutionImg(imageUrl, i) {
-  // Wenn das Bild nicht geladen werden kann, kann dies zu Problemen führen
-  const imgElement = document.getElementById(`evolution${i}`);
-  imgElement.src = imageUrl;
+async function displayImg(img) {
+    let imgContainerRef = document.getElementById("img-container-base")
+    imgContainerRef.innerHTML = `<img class="chain" id="evolution1" src="${img}" alt="1" />
+<span id="name-evo1"></span>`
 }
--- scripts/filter.js
+++ scripts/filter.js
@@ -1,63 +1,62 @@
 function filterPkmName() {
-  let inputRef = document.getElementById("input").value.toLowerCase();
-  let filteredPokemons = fullPokedex.filter((pokemon) => pokemon.name.toLowerCase().includes(inputRef));
-  filterActive = true;
-  if (inputRef.length >= 3) {
-    displayFilteredPokemons(filteredPokemons);
-    $(".dropdown-menu").removeClass("show");
-  } else {
-    init();
-  }
+    let inputRef = document.getElementById("input").value.toLowerCase();
+    let filteredPokemons = fullPokedex.filter((pokemon) => pokemon.name.toLowerCase().includes(inputRef));
+    filterActive = true;
+    if (inputRef.length >= 3) {
+        displayFilteredPokemons(filteredPokemons);
+        $(".dropdown-menu").removeClass("show");
+    } else {
+        init();
+    }
 }
 
 function filterPkmType(parameter) {
-  let typeRef = parameter.toLowerCase();
-  let filteredPokemons = fullPokedex.filter((pokemon) => {
-    filterActive = true;
-    for (let i = 0; i < pokemon.types.length; i++) {
-      if (pokemon.types[i].type.name.toLowerCase().includes(typeRef)) {
-        return true;
-      }
-    }
+    let typeRef = parameter.toLowerCase();
+    let filteredPokemons = fullPokedex.filter((pokemon) => {
+        filterActive = true;
+        for (let i = 0; i < pokemon.types.length; i++) {
+            if (pokemon.types[i].type.name.toLowerCase().includes(typeRef)) {
+                return true;
+            }
+        }
 
-    return false;
-  });
+        return false;
+    });
 
-  displayFilteredPokemons(filteredPokemons);
-  closeDropDown();
+    displayFilteredPokemons(filteredPokemons);
+    closeDropDown();
 }
 
 function displayFilteredPokemons(pokemons) {
-  let contentRef = document.getElementById("content");
-  contentRef.innerHTML = "";
+    let contentRef = document.getElementById("content");
+    contentRef.innerHTML = "";
 
-  if (pokemons.length === 0) {
-    contentRef.innerHTML = "Pokemon nicht gefunden";
-  } else {
-    for (let i = 0; i < pokemons.length; i++) {
-      let pokemon = pokemons[i];
-      let pokemonIndex = fullPokedex.indexOf(pokemon);
-      console.log(pokemon, pokemonIndex);
-      createPokemonCard(pokemonIndex);
-      displayPokemonSingleInformations(pokemonIndex, pokemon);
-      hideLoadMore();
+    if (pokemons.length === 0) {
+        contentRef.innerHTML = "Pokemon nicht gefunden";
+    } else {
+        for (let i = 0; i < pokemons.length; i++) {
+            let pokemon = pokemons[i];
+            let pokemonIndex = fullPokedex.indexOf(pokemon);
+            createPokemonCard(pokemonIndex);
+            displayPokemonSingleInformations(pokemonIndex, pokemon);
+            hideLoadMore();
+        }
     }
-  }
 }
 
 function closeDropDown() {
-  $("#drop-down-1").dropdown("toggle");
-  document.getElementById("drop-down-1").setAttribute("aria-expanded", false);
-  $("#drop-down-2").dropdown("toggle");
-  document.getElementById("drop-down-2").setAttribute("aria-expanded", false);
+    $("#drop-down-1").dropdown("toggle");
+    document.getElementById("drop-down-1").setAttribute("aria-expanded", false);
+    $("#drop-down-2").dropdown("toggle");
+    document.getElementById("drop-down-2").setAttribute("aria-expanded", false);
 }
 
 function hideLoadMore() {
-  let buttonRef = document.getElementById("load-more-btn");
-  buttonRef.classList.add("d-none");
+    let buttonRef = document.getElementById("load-more-btn");
+    buttonRef.classList.add("d-none");
 }
 
 function showLoadMore() {
-  let buttonRef = document.getElementById("load-more-btn");
-  buttonRef.classList.remove("d-none");
+    let buttonRef = document.getElementById("load-more-btn");
+    buttonRef.classList.remove("d-none");
 }
